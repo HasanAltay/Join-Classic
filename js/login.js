52b835123f2629ed1061a3f4a19001cfab4c86d2
HasanAltay <altay@posteo.de>
2022-11-10 12:18:36 +0100

    working navigation section intro logo animation imprint and privacy html added design updates

--- js/login.js
+++ js/login.js
@@ -1,14 +1,37 @@
+function init() {
+    login();
+    logoAnimation();
+}
+
+
+function logoAnimation() {
+    document.getElementById('animation').innerHTML = /*html*/`
+        <img src="./img/logo_dark.png" class="login_logo_animation">
+    `;
+}
+
+
 function login() {
     document.getElementById('login').innerHTML = /*html*/`
-    <div class="login_frame"> 
+    <div class="login_frame">
         <div></div>
         <div class="login_access">
-            <span>Log in</span><br><br>
+            <span>Log in</span><br>
             <div class="login_blue_line"></div><br><br>
-            <input placeholder="Email" id="email"><br><br>
-            <input placeholder="Password" id="password"><br><br>
-            <div>[ ] Remember me &nbsp;&nbsp;&nbsp;
-                <a href="#">Forgot my Password</a></div><br><br>
+            <div class="login_pos_rel">
+                <input placeholder="Email" id="email" type="email" class="login_input">
+                <br><br>
+                <input placeholder="Password" id="password" type="password" class="login_input">
+                <img src="./img/secure.png" class="login_secure_png">
+                <br>
+            </div>
+            <div class="login_forgot">
+                <div class="login_checkbox"></div>
+                <span>Remember me</span>
+                <a href="#">Forgot my Password</a>
+                <br><br><br>
+            </div>
+            <br>
             <div class="login_btns_dflex">
                 <button class="btn_dark" onclick="hideLogin()">Log in</button>
                 <button class="btn_bright" onclick="hideLogin()">Guest Log in</button>
@@ -21,4 +44,6 @@ function login() {
 
 function hideLogin() {
     document.getElementById('login').style.visibility = "hidden";
+    document.getElementById('animation').style.visibility = "hidden";
+    NavRenderSummary();
 }
--- js/nav_left.js
+++ js/nav_left.js
@@ -1,30 +1,100 @@
-function renderAddTask() {
-    document.getElementById('includeHTML').innerHTML = `
-    <div w3-include-html="./content/add_task.html"></div>
-    `;
-    includeHTML();
-}
-
-
-function renderContacts() {
-    document.getElementById('includeHTML').innerHTML = `
-    <div w3-include-html="./content/contacts.html"></div>
-    `;
-    includeHTML();
-}
-
-
-function renderSummary() {
+async function NavRenderSummary() {
     document.getElementById('includeHTML').innerHTML = `
     <div w3-include-html="./content/summary.html"></div>
     `;
-    includeHTML();
+    await includeHTML();
+    summaryPicked(); 
 }
 
 
-function renderBoard() {
+async function NavRenderBoard() {
     document.getElementById('includeHTML').innerHTML = `
     <div w3-include-html="./content/board.html"></div>
     `;
-    includeHTML();
+    await includeHTML();
+    boardPicked();
 }
+
+
+async function NavRenderAddTask() {
+    document.getElementById('includeHTML').innerHTML = `
+    <div w3-include-html="./content/add_task.html"></div>
+    `;
+    await includeHTML();
+    addTaskPicked();
+}
+
+
+async function NavRenderContacts() {
+    document.getElementById('includeHTML').innerHTML = `
+    <div w3-include-html="./content/contacts.html"></div>
+    `;
+    await includeHTML();
+    contactsPicked();
+}
+
+
+async function NavRenderPrivacy() {
+    document.getElementById('includeHTML').innerHTML = `
+    <div w3-include-html="./content/privacy.html"></div>
+    `;
+    await includeHTML();
+    privacyPicked();
+}
+
+
+async function NavRenderImprint() {
+    document.getElementById('includeHTML').innerHTML = `
+    <div w3-include-html="./content/imprint.html"></div>
+    `;
+    await includeHTML();
+    imprintPicked();
+}
+
+
+    function summaryPicked() {
+        document.getElementById('nav_summary').innerHTML = `
+        <div class="nav_btn_picked" onclick="renderSummary()">
+        <img src="../img/summary.png"><a>Summary</a></div>
+        `;
+    }
+
+
+    function boardPicked() {
+        document.getElementById('nav_board').innerHTML = `
+        <div class="nav_btn_picked" onclick="renderBoard()">
+        <img src="../img/board.png"><a>Board</a></div>
+        `;
+    }
+
+
+    function addTaskPicked() {
+        document.getElementById('nav_add_task').innerHTML = `
+        <div class="nav_btn_picked" onclick="renderAddTask()">
+        <img src="../img/add_task.png"><a>Add Task</a></div>
+        `;
+    }
+
+
+    function contactsPicked() {
+        document.getElementById('nav_contacts').innerHTML = `
+        <div class="nav_btn_picked" onclick="renderContacts()">
+        <img src="../img/contacts.png"><a>Contacts</a></div>
+        `;
+    }
+
+
+    function privacyPicked() {
+        document.getElementById('nav_privacy').innerHTML = `
+        <div class="nav_btn_picked" onclick="NavRenderprivacy()">
+        <img src="../img/info.png"><a>Privacy</a></div> 
+        `;
+    }
+
+
+    function imprintPicked() {
+        document.getElementById('nav_imprint').innerHTML = `
+        <div class="nav_btn_picked" onclick="NavRenderimprint()">
+        <img src="../img/info.png"><a>Imprint</a></div> 
+        `;
+    }
