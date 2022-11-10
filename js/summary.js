dcc61fbc8ee3d57d87ca0b84649e32f34f54ff64
HasanAltay <altay@posteo.de>
2022-11-10 19:59:34 +0100

    new images added for summary section

---
+++ js/summary.js
@@ -0,0 +1,7 @@
+function SumTodoChangeImage(img) {
+    document.getElementById('sum_todo_img').src = img;
+}
+
+function SumDoneChangeImage(img) {
+    document.getElementById('sum_done_img').src = img;
+}
