44501854e5c075831fbff6f1217dcd45badc5c28
HasanAltay <altay@posteo.de>
2022-11-11 13:08:33 +0100

    gen.updates

--- js/login.js
+++ js/login.js
@@ -13,19 +13,19 @@ function logoAnimation() {
 function login() {
     document.getElementById('login').innerHTML = /*html*/`
     <div class="login_frame">
-        <div></div>
         <div class="login_access">
             <span>Log in</span><br>
             <div class="login_blue_line"></div><br><br>
             <div class="login_pos_rel">
                 <input placeholder="Email" id="email" type="email" class="login_input">
+                <img src="./img/mail.png" class="login_mail_png">
                 <br><br>
                 <input placeholder="Password" id="password" type="password" class="login_input">
                 <img src="./img/secure.png" class="login_secure_png">
                 <br>
             </div>
             <div class="login_forgot">
-                <div class="login_checkbox"></div>
+                <input type="checkbox" class="login_checkbox">
                 <span>Remember me</span>
                 <a href="#">Forgot my Password</a>
                 <br><br><br>
@@ -35,8 +35,11 @@ function login() {
                 <button class="btn_dark" onclick="hideLogin()">Log in</button>
                 <button class="btn_bright" onclick="hideLogin()">Guest Log in</button>
             </div>
-        </div>        
-    </div>
+        </div> 
+        <div class="login_join">
+            <a>Not a join user?</a><button class="login_btn_join">Sign up</button>
+        </div>       
+    </div> 
     `;
 }
 
