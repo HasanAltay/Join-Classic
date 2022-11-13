c90c0488ea9754abe57c2ca29c76c532f836d886
Yehoyakim88 <yehoyakim@mail.de>
2022-11-13 18:05:39 +0100

    contactList update. contacts with labels /wo color

--- js/contacts.js
+++ js/contacts.js
@@ -1,45 +1,203 @@
 let contacts = [
-    
-    {"name": "John",
-     "phone" : "+49123456789",
-     "mail" : "abcd@mail.de"    
-},
-
-    {"name": "Anna",
-     "phone" : "+49123456789",
-     "mail" : "abcd@mail.de"    
-},
-
-    {"name": "Max",
-     "phone" : "+49123456789",
-     "mail" : "abcd@mail.de"    
-},
-
-
-    {"name": "Susan",
-     "phone" : "+49123456789",
-     "mail" : "abcd@mail.de"    
-}
-
+  {
+    letter: "A",
+    names: ["Anna", "Alina"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "B",
+    names: ["Bernd", "Berth"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "C",
+    names: ["Cora", "Connor"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "D",
+    names: ["Daniel", "David"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "E",
+    names: ["Emil", "Emily"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "F",
+    names: ["Frank", "Fred"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "G",
+    names: ["Georg", "Gustav"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "H",
+    names: ["Hans", "Houston"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "I",
+    names: ["Ines", "Ingo"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "J",
+    names: ["Josef", "Johanna"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "K",
+    names: ["Kevin", "Klara"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "L",
+    names: ["Lars", "Laura"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "M",
+    names: ["Max", "Mara"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "N",
+    names: ["Nala", "Norbert"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "O",
+    names: ["Olaf", "Oskar"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "P",
+    names: ["Paul", "Philipp"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "Q",
+    names: ["Quatsch", "Qualle"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "R",
+    names: ["Richard", "Rebekka"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "S",
+    names: ["Simon", "Susi"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "T",
+    names: ["Theodor", "Thomas"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "U",
+    names: ["Ulrich", "Ulla"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "V",
+    names: ["Victor", "Verena"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "W",
+    names: ["Wolfgang", "Walther"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "X",
+    names: ["Xavier", "Xianthi"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "Y",
+    names: ["Yake"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
+  {
+    letter: "Z",
+    names: ["Zara", "Zephanja"],
+    lastNames: ["Dobai", "Numey"],
+    phonenumbers: ["01599 12345678", "01599 12345679"],
+    mail: ["zara@mail.de", "zehanja@mail.de"],
+  },
 ];
 
-
 async function initContacts() {
-    console.log('initContacts()');
-    document.getElementById('contacts-id').innerHTML += drawContactHTML();
-    console.log(contacts['John']);
-    console.log(`contacts.length: ${contacts.length}`);
+  console.log("initContacts()");
+  document.getElementById("contacts-id").innerHTML += drawContactHTML();
+  console.log(contacts["John"]);
+  console.log(`contacts.length: ${contacts.length}`);
 
-    for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        insertContactToContactList(i, contact);        
-    }
+  for (let i = 0; i < contacts.length; i++) {
+    const contact = contacts[i];
+    insertContactToContactList(i, contact);
+  }
 }
 
-
 function drawContactHTML() {
-    console.log('drawContactHTML()');
-    return /*html*/`
+  console.log("drawContactHTML()");
+  return /*html*/ `
     <div id="contacts-list-container" class="con_contactsListContainer">
         <div id="contact-list" class="con_contactList"></div>
     </div>
@@ -55,8 +213,22 @@ function drawContactHTML() {
     `;
 }
 
-
 function insertContactToContactList(i, contact) {
-    console.log('insertContactToContactList');
-    document.getElementById('contact-list').innerHTML += `<span>${contact['name']}</span>`;
+  console.log("insertContactToContactList");
+  document.getElementById(
+    "contact-list"
+  ).innerHTML += `<span id="${contact["letter"]} " class="con_alphabeticHints">${contact["letter"]}</span>`;
+
+  for (let i = 0; i < contact["names"].length; i++) {
+    const element = contact["names"][i];
+    const element2 = contact["lastNames"][i];
+    const fName = element.charAt(0);
+    const lName = element2.charAt(0);
+    document.getElementById(
+      "contact-list"
+    ).innerHTML += `<div id="${contact["letter"]}-${i}" class="con_contactListElement">
+    <span class="con_contactListElementLabel">${fName}${lName}</span>
+    <span>${element} ${element2}</span>
+    </div>`;
+  }
 }
