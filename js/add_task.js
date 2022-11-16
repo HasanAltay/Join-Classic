a7c98e427dc04f73f269ebd395bd3596000f526a
Christian Axtmann <christian.axtmann@posteo.de>
2022-11-16 15:34:33 +0100

    addSubtask

--- js/add_task.js
+++ js/add_task.js
@@ -18,15 +18,60 @@ function inactiveMode(img) {
     console.log('inactive');
 }
 
+let tasks = [];
+
 function addTask() {
-    let task = document.getElementById('task').innerHTML;
-    let category = document.getElementById('category').innerHTML;
+    let title = document.getElementById('title');
+    let description = document.getElementById('description');
+    let category = document.getElementById('task').innerHTML;
+    let contacts = document.getElementById('assign').innerHTML;
+
+    let task = {
+        "title": title.value,
+        "description": description.value,
+        "category": category,
+        "contacts": contacts
+    };
+
+    tasks.push(task);
+    console.log(tasks);
+    title.value='';
+    description.value='';
+}
+
+function clear_subtaskInput() {
+    let typeSubtask = document.getElementById('img-replace');
+    typeSubtask.innerHTML = /*html*/`
+    <img class="img-cancelSubtask" src='./img/subtask-cancel.png' onclick="clearInputSubtask()">
+    <img src="./img/vertical.png">
+    <img class="img-addSubtask" src='./img/addSubtask.png' onclick="showSubtask()">
+    `;
+}
+
+function clearInputSubtask() {
+    let input = document.getElementById('inputSubtask');
+    input.value='';
+    showDefaultSubtask();
 }
 
 function showSubtask() {
-    let subtask = document.getElementById('input_subtask');
-    document.getElementById('ul_subtask').innerHTML+=`<ul>${subtask.value}</ul>`;
-    subtask.value = '';
-
+    let input = document.getElementById('inputSubtask');
+    let subtask = document.getElementById('list_subtask');
+    if(input.value == '') {return 0}
+    else {
+        subtask.innerHTML += /*html*/`
+        <li><input class="checkbox" type="checkbox"><span class="subtask-item">${input.value}</span></li>`;
+    }
+    clearInputSubtask();
 }
 
+function showDefaultSubtask() {
+    let defaultSubtask = document.getElementById('img-replace');
+    defaultSubtask.innerHTML = /*html*/`
+    <img id="img-subtask" class="plus-icon" src="./img/plus.png">
+    `;
+}
+
+function showCategory() {
+    document.getElementById('contentCategory').classList.remove('dropdown-content');
+}
