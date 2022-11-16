faea4d9fa8595467a1d2c616c14d0bbd78f1d71d
Yehoyakim88 <yehoyakim@mail.de>
2022-11-16 09:35:17 +0100

    added feature contactElement toggle bgColor onclick()

--- js/contacts.js
+++ js/contacts.js
@@ -4,175 +4,175 @@ let contacts = [
     names: ["Anna", "Alina"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["anna_dobai@mail.de", "alina_numey@mail.de"]
+    mail: ["anna_dobai@mail.de", "alina_numey@mail.de"],
   },
   {
     letter: "B",
     names: ["Bernd", "Berth"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["bernd_dobai@mail.de", "berth_numey@mail.de"]
+    mail: ["bernd_dobai@mail.de", "berth_numey@mail.de"],
   },
   {
     letter: "C",
     names: ["Cora", "Connor"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["cora_dobai@mail.de", "connor_numey@mail.de"]
+    mail: ["cora_dobai@mail.de", "connor_numey@mail.de"],
   },
   {
     letter: "D",
     names: ["Daniel", "David"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["daniel_dobai@mail.de", "david_numey@mail.de"]
+    mail: ["daniel_dobai@mail.de", "david_numey@mail.de"],
   },
   {
     letter: "E",
     names: ["Emil", "Emily"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["emil_dobai@mail.de", "emily_numey@mail.de"]
+    mail: ["emil_dobai@mail.de", "emily_numey@mail.de"],
   },
   {
     letter: "F",
     names: ["Frank", "Fred"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["frank_dobai@mail.de", "fred_numey@mail.de"]
+    mail: ["frank_dobai@mail.de", "fred_numey@mail.de"],
   },
   {
     letter: "G",
     names: ["Georg", "Gustav"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["georg_dobai@mail.de", "gustav_numey@mail.de"]
+    mail: ["georg_dobai@mail.de", "gustav_numey@mail.de"],
   },
   {
     letter: "H",
     names: ["Hans", "Houston"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["hans_dobai@mail.de", "houston_numey@mail.de"]
+    mail: ["hans_dobai@mail.de", "houston_numey@mail.de"],
   },
   {
     letter: "I",
     names: ["Ines", "Ingo"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["ines_dobai@mail.de", "ingo_numey@mail.de"]
+    mail: ["ines_dobai@mail.de", "ingo_numey@mail.de"],
   },
   {
     letter: "J",
     names: ["Josef", "Johanna"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["josef_dobai@mail.de", "johanna_numey@mail.de"]
+    mail: ["josef_dobai@mail.de", "johanna_numey@mail.de"],
   },
   {
     letter: "K",
     names: ["Kevin", "Klara"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["kevin_dobai@mail.de", "klara_numey@mail.de"]
+    mail: ["kevin_dobai@mail.de", "klara_numey@mail.de"],
   },
   {
     letter: "L",
     names: ["Lars", "Laura"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["lars_dobai@mail.de", "laura_numey@mail.de"]
+    mail: ["lars_dobai@mail.de", "laura_numey@mail.de"],
   },
   {
     letter: "M",
     names: ["Max", "Mara"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["max_dobai@mail.de", "mara_numey@mail.de"]
+    mail: ["max_dobai@mail.de", "mara_numey@mail.de"],
   },
   {
     letter: "N",
     names: ["Nala", "Norbert"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["nala_dobai@mail.de", "norbert_numey@mail.de"]
+    mail: ["nala_dobai@mail.de", "norbert_numey@mail.de"],
   },
   {
     letter: "O",
     names: ["Olaf", "Oskar"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["olaf_dobai@mail.de", "oskar_numey@mail.de"]
+    mail: ["olaf_dobai@mail.de", "oskar_numey@mail.de"],
   },
   {
     letter: "P",
     names: ["Paul", "Philipp"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["paul_dobai@mail.de", "philipp_numey@mail.de"]
+    mail: ["paul_dobai@mail.de", "philipp_numey@mail.de"],
   },
   {
     letter: "Q",
     names: ["Quatsch", "Qualle"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["quatsch_dobai@mail.de", "qualle_numey@mail.de"]
+    mail: ["quatsch_dobai@mail.de", "qualle_numey@mail.de"],
   },
   {
     letter: "R",
     names: ["Richard", "Rebekka"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["richard_dobai@mail.de", "rebekka_numey@mail.de"]
+    mail: ["richard_dobai@mail.de", "rebekka_numey@mail.de"],
   },
   {
     letter: "S",
     names: ["Simon", "Susi"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["simon_dobai@mail.de", "susi_numey@mail.de"]
+    mail: ["simon_dobai@mail.de", "susi_numey@mail.de"],
   },
   {
     letter: "T",
     names: ["Theodor", "Thomas"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["theodor_dobai@mail.de", "thomas_numey@mail.de"]
+    mail: ["theodor_dobai@mail.de", "thomas_numey@mail.de"],
   },
   {
     letter: "U",
     names: ["Ulrich", "Ulla"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["ulrich_dobai@mail.de", "ulla_numey@mail.de"]
+    mail: ["ulrich_dobai@mail.de", "ulla_numey@mail.de"],
   },
   {
     letter: "V",
     names: ["Victor", "Verena"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["victor_dobai@mail.de", "verena_numey@mail.de"]
+    mail: ["victor_dobai@mail.de", "verena_numey@mail.de"],
   },
   {
     letter: "W",
     names: ["Wolfgang", "Walther"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["wolfgang_dobai@mail.de", "walther_numey@mail.de"]
+    mail: ["wolfgang_dobai@mail.de", "walther_numey@mail.de"],
   },
   {
     letter: "X",
     names: ["Xavier", "Xianthi"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["xavier_dobai@mail.de", "xianthi_numey@mail.de"]
+    mail: ["xavier_dobai@mail.de", "xianthi_numey@mail.de"],
   },
   {
     letter: "Y",
     names: ["Yake", "Yuri"],
     lastNames: ["Dobai", "Numey"],
     phonenumbers: ["01599 12345678", "01599 12345679"],
-    mail: ["yake_dobai@mail.de", "yuri_numey@mail.de"]
+    mail: ["yake_dobai@mail.de", "yuri_numey@mail.de"],
   },
   {
     letter: "Z",
@@ -187,6 +187,7 @@ var labelColors;
 
 var currentLabelColor;
 
+var last_id;
 
 function initGlobalVariables() {
   currentLabelColor = 0;
@@ -204,10 +205,11 @@ function initGlobalVariables() {
     BG_COLOR_JAVA,
     BG_COLOR_PURPLE_PIZZAZZ,
     BG_COLOR_HELIOTROPE,
-    BG_COLOR_ELECTRIC_VIOLET
+    BG_COLOR_ELECTRIC_VIOLET,
   ];
-}
 
+  last_id = "XX";
+}
 
 function initContacts() {
   console.log("initContacts()");
@@ -218,7 +220,6 @@ function initContacts() {
     insertContactToContactList(i, contact);
   }
 }
-
 
 function insertContactToContactList(i, contact) {
   console.log("insertContactToContactList");
@@ -234,44 +235,50 @@ function insertContactToContactList(i, contact) {
     const fName = element.charAt(0);
     const lName = element2.charAt(0);
 
-    if(currentLabelColor >= labelColors.length) {
+    if (currentLabelColor >= labelColors.length) {
       currentLabelColor = 0;
     }
-   
+
     const bgColor = labelColors[currentLabelColor];
     currentLabelColor++;
-    
-    document.getElementById(`${contact["letter"]}`).innerHTML +=
-    `<div id="${contact["letter"]}-${i}" class="con_contactListElement hoverEffect">
-      <span id="label-${contact["letter"]}-${i}" class="con_contactListElementLabel">${fName}${lName}</span> .  
+    let given_id = `${contact["letter"]}-${i}`;
+
+    document.getElementById(
+      `${contact["letter"]}`
+    ).innerHTML += `<div id="${contact["letter"]}-${i}" class="con_contactListElement hoverEffect" onclick="contactClicked(id)">
+      <span id="label-${contact["letter"]}-${i}" class="con_contactListElementLabel">${fName}${lName}</span>  
       <div>
         <span>${element} ${element2}</span>
         <span class="con_contactListElementEmail">${element3}</span>
       </div>
       
     </div>`;
-    document.getElementById(`label-${contact["letter"]}-${i}`).style.backgroundColor = bgColor;
+    document.getElementById(
+      `label-${contact["letter"]}-${i}`
+    ).style.backgroundColor = bgColor;
   }
 }
 
-
 function newContact() {
-  alert('You wanna add new contact?!');
+  alert("You wanna add new contact?!");
 }
-
 
 function addNewContact(contactName, contactPhone, contactMail) {
   let newContactFirstName = contactName.split(" ")[0];
   let newContactLastName = contactName.split(" ")[1];
   let firstLetterFirstName = newContactFirstName.charAt(0);
   let firstLetterLastName = newContactLastName.charAt(0);
-  let currentNumberOfContactsAt = contacts[firstLetterOfNewContact]['names'].length;
+  let currentNumberOfContactsAt =
+    contacts[firstLetterOfNewContact]["names"].length;
 
-  contacts[firstLetterOfNewContact]['names'].push(newContactFirstName);
-  contacts[firstLetterOfNewContact]['lastNames'].push(newContactFirstName);
+  contacts[firstLetterOfNewContact]["names"].push(newContactFirstName);
+  contacts[firstLetterOfNewContact]["lastNames"].push(newContactFirstName);
 
-  document.getElementById(`${firstLetterFirstName}`).innerHTML += 
-    `<div id="${firstLetterOfNewContact}-${currentNumberOfContactsAt+1}" class="con_contactListElement hoverEffect">
+  document.getElementById(
+    `${firstLetterFirstName}`
+  ).innerHTML += `<div id="${firstLetterOfNewContact}-${
+    currentNumberOfContactsAt + 1
+  }" class="con_contactListElement hoverEffect">
       <span class="con_contactListElementLabel">${firstLetterFirstName}${firstLetterLastName}</span>
       <div>
         <span>${newContactFirstName} ${newContactLastName}</span>
@@ -280,11 +287,30 @@ function addNewContact(contactName, contactPhone, contactMail) {
       
     </div>`;
 
-
-
-
-
-
   // document.getElementById('')
   currentLabelColor++;
+  let a = String();
+}
+
+function contactClicked(given_id) {
+  console.log("contactClicked()");
+  console.log(`last_id: ${last_id}`);
+  console.log(`given_id: ${given_id}`);
+
+  if (given_id != last_id) {
+      console.log("given_id != last_id");
+      document.getElementById(given_id).style.backgroundColor = "#2A3647";
+      document.getElementById(given_id).style.color = "#FFFFFF";
+    } else if (given_id == last_id) {
+      document.getElementById(given_id).style.backgroundColor = "#FFFFFF";
+      document.getElementById(given_id).style.color = "#000000";
+    }
+
+    if(last_id != "XX") {
+      document.getElementById(last_id).style.backgroundColor = "#FFFFFF";
+      document.getElementById(last_id).style.color = "#000000";
+    }
+  
+  last_id = given_id;
+  console.log(`last_id: ${last_id}`);
 }
