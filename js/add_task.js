de4c83b02c498a57b6ce98073b4d6b103e0d140b
HasanAltay <altay@posteo.de>
2023-02-15 22:02:37 +0100

    add task input fields

--- js/add_task.js
+++ js/add_task.js
@@ -1,4 +1,83 @@
+let tasks = [];
+
 function SetPriority(num, set) {
   console.log(num);
   console.log(set);
+
+  let urgent = document.getElementById("urgent");
+  let medium = document.getElementById("medium");
+  let low = document.getElementById("low");
+
+  let urgent_img = document.getElementById("urgent_img");
+  let medium_img = document.getElementById("medium_img");
+  let low_img = document.getElementById("low_img");
+
+  if (num == 1) {
+    urgent.style.backgroundColor = "#FF3D00";
+    medium.style.backgroundColor = "#FFFFFF";
+    low.style.backgroundColor = "#FFFFFF";
+
+    urgent.style.color = "#FFFFFF";
+    medium.style.color = "#000000";
+    low.style.color = "#000000";
+
+    urgent_img.style.filter = "brightness(0) invert(1)";
+    medium_img.style.filter = "unset";
+    low_img.style.filter = "unset";
+  }
+  else if (num == 2) { 
+    urgent.style.backgroundColor = "#FFFFFF";
+    medium.style.backgroundColor = "#FFA800";
+    low.style.backgroundColor = "#FFFFFF";
+    
+    urgent.style.color = "#000000";
+    medium.style.color = "#FFFFFF";
+    low.style.color = "#000000";
+
+    urgent_img.style.filter = "unset";
+    medium_img.style.filter = "brightness(0) invert(1)";
+    low_img.style.filter = "unset";
+  }
+  else if (num == 3) { 
+    urgent.style.backgroundColor = "#FFFFFF";
+    medium.style.backgroundColor = "#FFFFFF";
+    low.style.backgroundColor = "#7AE229";
+
+    urgent.style.color = "#000000";
+    medium.style.color = "#000000";
+    low.style.color = "#FFFFFF";
+
+    urgent_img.style.filter = "unset";
+    medium_img.style.filter = "unset";
+    low_img.style.filter = "brightness(0) invert(1)";
+  }
+}
+
+
+function addTask() {
+	event.preventDefault();
+
+  let title = document.getElementById('title').value;
+  let textarea = document.getElementById('textarea').value;
+  let date = document.getElementById('date').value;
+
+  console.log(title, textarea, date);
+  tasks.push({
+    "Titel" : title, 
+    "Textarea" : textarea, 
+    "Date" : date
+  });
+  console.log(tasks);
+}
+
+
+function onFormSubmit() {
+	event.preventDefault();
+	// your Javascript code here
+}
+
+
+function clearForm(formId) {
+  const form = document.getElementById(formId);
+  form.reset();
 }
--- js/contacts.js
+++ js/contacts.js
@@ -1,71 +1,72 @@
 let contacts;
 let sortedContacts = {};
 
+
 async function fetchContacts() {
-  resetList();
-  fetch('/json/contacts.json')
-    .then(data => data.json())
-    .then(data => {
-      // sorts the json array by the first letter of the first entry.
-      data.sort(function (a, b) {
-        let nameA = a.name.charAt(0).toLowerCase();
-        let nameB = b.name.charAt(0).toLowerCase();
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-      contacts = data;
-      initList();
-    });
+    resetList();
+    fetch("/json/contacts.json")
+        .then(data => data.json())
+        .then(data => {
+            // sorts the json array by the first letter of the first entry.
+            data.sort(function (a, b) {
+                let nameA = a.name.charAt(0).toLowerCase();
+                let nameB = b.name.charAt(0).toLowerCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+            contacts = data;
+            initList();
+        });
 }
 
 
 function initList() {
-  for (let i = 0; i < contacts.length; i++) {
-    let firstLetter = contacts[i].name[0].toUpperCase();
-    if (!sortedContacts[firstLetter]) {
-      sortedContacts[firstLetter] = [];
+    for (let i = 0; i < contacts.length; i++) {
+        let firstLetter = contacts[i].name[0].toUpperCase();
+        if (!sortedContacts[firstLetter]) {
+            sortedContacts[firstLetter] = [];
+        }
+        sortedContacts[firstLetter].push(contacts[i]);
     }
-    sortedContacts[firstLetter].push(contacts[i]);
-  }
-  for (let letter in sortedContacts) {
-    let letter_box = document.getElementById('letter_box');
-    letter_box.innerHTML += `
+    for (let letter in sortedContacts) {
+        let letter_box = document.getElementById("letter_box");
+        letter_box.innerHTML += `
       <a class="contacts_letters">${letter}</a>
     `;
-    for (let i = 0; i < sortedContacts[letter].length; i++) {
-      let name_letter = sortedContacts[letter][i].name.slice(0, 1);
-      let surname_letter = sortedContacts[letter][i].surname.slice(0, 1);
-      let letters = name_letter + surname_letter;
-      let name = sortedContacts[letter][i].name;
-      let surname = sortedContacts[letter][i].surname;
-      let mail = sortedContacts[letter][i].mail;
-      let phone = sortedContacts[letter][i].phone;
-      const color = getColor(letters);
-      console.log(color);
-      letter_box.innerHTML += /*html*/`
-        <div class="contacts_files" id="files" 
+        for (let i = 0; i < sortedContacts[letter].length; i++) {
+            let name_letter = sortedContacts[letter][i].name.slice(0, 1);
+            let surname_letter = sortedContacts[letter][i].surname.slice(0, 1);
+            let letters = name_letter + surname_letter;
+            let name = sortedContacts[letter][i].name;
+            let surname = sortedContacts[letter][i].surname;
+            let mail = sortedContacts[letter][i].mail;
+            let phone = sortedContacts[letter][i].phone;
+            const color = getColor(letters);
+            console.log(color);
+            letter_box.innerHTML += /*html*/ `
+        <button class="contacts_files" id="files" 
           onclick="initDetails('${letters}','${name}','${surname}','${mail}','${phone}','${color}');showDetails()">
           <div class="contacts_initials" id="initials" style="background-color:${color}">${letters}</div>
           <div class="contacts_name_email" id="name_email">
-            <div>${name} ${surname}</div>
-            <div>${mail}</div>
+            <span>${name} ${surname}</span>
+            <a>${mail}</a>
           </div>
-        </div>
+        </button>
       `;
+        }
     }
-  }
 }
 
 
 function initDetails(letters, name, surname, mail, phone, color) {
-  console.log(letters, name, mail, phone);
-  let contacts_details = document.getElementById('contacts_details');
-  contacts_details.innerHTML = `
+    console.log(letters, name, mail, phone);
+    let contacts_details = document.getElementById("contacts_details");
+    contacts_details.innerHTML = `
     <div class="details_header">
       <div class="details_initials" style="background-color:${color}">${letters}</div>
       <span class="details_name">${name} ${surname}</span>
@@ -83,52 +84,141 @@ function initDetails(letters, name, surname, mail, phone, color) {
     </div>
     <button class="button_dark new_contact_pos" onclick="showAddContact()">Add contact</button>
     <img class="mobile_arrow" src="/img/left_arrow_blue.png" onclick="closeDetails()">
-    <div class="contact_new" id="contact_new">
+    <div class="contact_new" id="contact_new"></div>
+  `;
+    addContact();
+}
+
+
+function addContact() {
+    let contact_new = document.getElementById("contact_new");
+    contact_new.innerHTML = `
       <div class="contact_new_top">
         <img src="/img/logo_topbar.png">
         <span>Add contact</span>
         <a>Tasks are better with a team!</a>
       </div>
-      <div class="contact_new_bottom">
-        <input type="text" placeholder="Name" name="name" maxlength="36">
-        <input type="text" placeholder="Surname" name="surname" maxlength="36">
-        <input type="text" placeholder="Email" name="mail" maxlength="36">
-        <input type="text" placeholder="Phone" name="phone" maxlength="36">
+      <form class="contact_new_bottom" id="myForm" onsubmit="onFormSubmit();">
+
+        <input type="text" placeholder="Name" name="name" maxlength="36" id="name">
+        <input type="text" placeholder="Surname" name="surname" maxlength="36" id="surname">
+        <input type="email" placeholder="Email" name="mail" maxlength="36" id="mail">
+        <input type="number" placeholder="Phone" name="phone" maxlength="16" id="phone">
+
         <div class="contact_new_btns">
           <button class="button_bright" onclick="closeAddContact()">Cancel</button>
-          <button class="button_dark">Add</button>
+          <input type="submit" class="button_dark" onclick="submitContact()" value="Add">
         </div>
-      </div>
-    </div>
+      </form>
   `;
 }
 
 
 function resetList() {
-  contacts = [];
-  sortedContacts = [];
+    contacts = [];
+    sortedContacts = [];
 }
 
 
 function showDetails() {
-  let details = document.getElementById("contacts_details");
-  details.style.display = "flex";
+    let details = document.getElementById("contacts_details");
+    details.style.display = "flex";
 }
 
 
 function closeDetails() {
-  let details = document.getElementById("contacts_details");
-  details.style.display = "none";
+    let details = document.getElementById("contacts_details");
+    details.style.display = "none";
 }
 
 
 function showAddContact() {
-  let contact_new = document.getElementById("contact_new");
-  contact_new.style.display = "block";
+    let contact_new = document.getElementById("contact_new");
+    contact_new.style.display = "block";
 }
 
 
 function closeAddContact() {
-  let contact_new = document.getElementById("contact_new");
-  contact_new.style.display = "none";
+    let contact_new = document.getElementById("contact_new");
+    contact_new.style.display = "none";
 }
+
+
+
+
+// function submitContact() {
+//     let myForm = document.getElementById("myForm");
+//     myForm.addEventListener("submit", function (event) {
+//         event.preventDefault();
+
+//         let name = document.getElementById("name").value;
+//         let surname = document.getElementById("surname").value;
+//         let mail = document.getElementById("mail").value;
+//         let phone = document.getElementById("phone").value;
+
+//         // create an object with the new data
+//         let newData = {
+//             name: name,
+//             surname: surname,
+//             mail: mail,
+//             phone: phone,
+//         };
+
+//         // send a POST request to the server to update the JSON file
+//         fetch("/json/contacts.json", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(newData),
+//         })
+//             .then(response => response.json())
+//             .then(data => console.log(data))
+//             .catch(error => console.error(error));
+//     });
+
+//     // const http = require("http");
+//     // const fs = require("fs");
+
+//     http.createServer((req, res) => {
+//         if (req.method === "POST" && req.url === "/json/contacts.json") {
+//             let body = "";
+
+//             req.on("data", chunk => {
+//                 body += chunk;
+//             });
+
+//             req.on("end", () => {
+//                 let newData = JSON.parse(body);
+
+//                 // read the existing data from the JSON file
+//                 fs.readFile("/json/contacts.json", (err, data) => {
+//                     if (err) {
+//                         res.statusCode = 500;
+//                         res.end("Error reading data file");
+//                         return;
+//                     }
+
+//                     let jsonData = JSON.parse(data);
+
+//                     // add the new data to the existing data
+//                     jsonData.push(newData);
+
+//                     // write the updated data back to the JSON file
+//                     fs.writeFile("/json/contacts.json", JSON.stringify(jsonData), err => {
+//                         if (err) {
+//                             res.statusCode = 500;
+//                             res.end("Error writing data file");
+//                             return;
+//                         }
+
+//                         res.setHeader("Content-Type", "application/json");
+//                         res.end(JSON.stringify(jsonData));
+//                     });
+//                 });
+//             });
+//         }
+//     }).listen(3000, () => {
+//         console.log("Server running on port 3000");
+//     });
+// }
