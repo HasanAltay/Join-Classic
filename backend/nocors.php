4ccf91594fb11016db50c276fd50b0c6676c5fb5
HasanAltay <altay@posteo.de>
2023-02-19 17:19:45 +0100

    color picker icon json array backend data for add task drinking a tea and hoping the best

--- backend/nocors.php
+++ backend/nocors.php
@@ -2,8 +2,6 @@
 cors();
 echo file_get_contents($_GET['json'] . '.json');
 
-
-
 function cors()
 {
 
@@ -14,7 +12,7 @@ function cors()
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    } 
+    }
 
 
     header("Access-Control-Allow-Origin: *");
