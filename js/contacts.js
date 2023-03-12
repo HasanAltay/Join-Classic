2b06a42676860b170cc9ca5916e036eb3e79fef8
HasanAltay <altay@posteo.de>
2023-03-12 15:06:47 +0100

    working contacts: - add new contact - edit contact

--- js/contacts.js
+++ js/contacts.js
@@ -1,12 +1,13 @@
 let contacts = [];
 let sortedContacts = {};
 let contactsListTasks = [];
+let pickedContact;
 
 function saveNewContact() {
-    const add_name = document.getElementById("name").value;
-    const add_surname = document.getElementById("surname").value;
-    const add_mail = document.getElementById("mail").value;
-    const add_phone = document.getElementById("phone").value;
+    const add_name = document.getElementById("add_name").value;
+    const add_surname = document.getElementById("add_surname").value;
+    const add_mail = document.getElementById("add_mail").value;
+    const add_phone = document.getElementById("add_phone").value;
     const new_contact = {
         name: add_name,
         surname: add_surname,
@@ -14,17 +15,29 @@ function saveNewContact() {
         mail: add_mail,
     };
     contacts.push(new_contact);
-
-    // console.log(contacts);
-
     backend.saveContacts("contacts", JSON.stringify(contacts));
     console.log("Server:", jsonContactsFromServer.contacts);
     // downloadFromServer();
     // initLettersFromContacts();
 }
 
+function editExistingContact() {
+    const edit_name = document.getElementById("edit_name").value;
+    const edit_surname = document.getElementById("edit_surname").value;
+    const edit_mail = document.getElementById("edit_mail").value;
+    const edit_phone = document.getElementById("edit_phone").value;
+    const edited_contact = {
+        name: edit_name,
+        surname: edit_surname,
+        phone: edit_phone,
+        mail: edit_mail,
+    };
+    contacts[pickedContact] = edited_contact;
+    backend.saveContacts("contacts", JSON.stringify(contacts));
+}
+
 async function initLettersFromContacts() {
-  // console.log(typeof contacts);
+    // console.log(typeof contacts);
     if (contacts.length > 0) {
         contacts.sort(function (a, b) {
             let nameA = (a.name || "").charAt(0).toLowerCase();
@@ -39,7 +52,6 @@ async function initLettersFromContacts() {
         });
         sortedContacts = {}; // clear sortedContacts object
         letter_box.innerHTML = ""; // clear letter_box element
-        // console.log(contacts);
         initList(contacts);
     }
 }
@@ -48,13 +60,11 @@ function initList(contacts) {
     contactsListTasks = [];
     let letter_box = document.getElementById("letter_box");
     for (let i = 0; i < contacts.length; i++) {
-        // console.log(contacts);
         const firstLetter = contacts[i].name[0].toUpperCase();
         if (!sortedContacts[firstLetter]) {
             sortedContacts[firstLetter] = [];
         }
         sortedContacts[firstLetter].push(contacts[i]);
-        // console.log(firstLetter);
     }
     for (let letter in sortedContacts) {
         letter_box.innerHTML += `
@@ -93,10 +103,14 @@ function initDetails(letters, name, surname, mail, phone, color) {
     contacts_details.innerHTML = `
     <div class="details_header">
       <div class="details_initials" style="background-color:${color}">${letters}</div>
-      <span class="details_name">${name} ${surname}<br><a><img src="./img/plus_blue.png">Add Task</a></span>
+      <span class="details_name">${name} ${surname}<br>
+        <a><img src="./img/plus_blue.png">Add Task</a>
+      </span>
     </div>
     <div>
-      <span class="contacts_h2">Contact Information<a onclick="showEditContact()"><img src="./img/pen_blue.png">Edit Contact</a><br><br></span>
+      <span class="contacts_h2">Contact Information<a onclick="showEditContact()">
+        <img src="./img/pen_blue.png">Edit Contact</a><br><br>
+      </span>
     </div>
     <div>
       <b>Email<br><br></b>
@@ -106,16 +120,19 @@ function initDetails(letters, name, surname, mail, phone, color) {
       <b>Phone<br><br></b>
       <a href="tel:${phone}">${phone}</a>
     </div>
-    <button class="button_dark new_contact_pos" onclick="showAddContact()">Add contact<img src="./img/add_contact.png"></button>
+    <button class="button_dark new_contact_pos" onclick="showAddContact()">Add contact
+      <img src="./img/add_contact.png">
+    </button>
     <img class="mobile_arrow" src="./img/left_arrow.png" onclick="closeDetails()">
   `;
     addContact();
-    editContact();
+    editContact(name, surname, mail, phone);
     // console.log(contacts);
 }
 
 // adds new contact to the list
 function addContact() {
+    let msg = "New Contact added!";
     let contact_new = document.getElementById("contact_new");
     contact_new.innerHTML = `
     <div class="contact_new_top">
@@ -125,20 +142,34 @@ function addContact() {
       <a>Tasks are better with a team!</a> 
     </div> 
     <form class="contact_new_bottom" id="add_contact" onsubmit="onFormSubmit();"> 
-      <input type="text" placeholder="Name" name="name" maxlength="36" id="name"> 
-      <input type="text" placeholder="Surname" name="surname" maxlength="36" id="surname"> 
-      <input type="email" placeholder="Email" name="mail" maxlength="36" id="mail"> 
-      <input type="tel" placeholder="Phone" name="phone" id="phone" pattern="[0-9]{10,16}" title="Please enter a valid phone number (between 10 and 16 digits)"> 
+      <input type="text" placeholder="Name" name="name" maxlength="36" id="add_name"> 
+      <input type="text" placeholder="Surname" name="surname" maxlength="36" id="add_surname"> 
+      <input type="email" placeholder="Email" name="mail" maxlength="36" id="add_mail"> 
+      <input type="tel" placeholder="Phone" name="phone" id="add_phone" 
+        pattern="[0-9]{10,16}" title="Please enter a valid phone number (between 10 and 16 digits)"> 
       <div class="contact_new_btns"> 
         <button class="button_bright" onclick="closeAddContact()">Cancel</button> 
-        <button type="submit" class="button_dark" onclick="saveNewContact();showConfirmationAddContact(a=true);" value="Add">Add</button> 
+        <button type="submit" class="button_dark" 
+          onclick="saveNewContact();
+            showConfirmationContact(true,'${msg}');">Add
+        </button> 
       </div> 
     </form>
   `;
 }
 
 // edit existing contact from the list
-function editContact() {
+function editContact(name, surname, mail, phone) {
+    // finds the position of the picked contact for editing
+    const firstLetter = name.charAt(0).toUpperCase();
+    const contactsStartingWithLetter = sortedContacts[firstLetter];
+    for (let i = 0; i < contactsStartingWithLetter.length; i++) {
+        const contact = contactsStartingWithLetter[i];
+        if (contact.name === name && contact.surname === surname) {
+            pickedContact = contacts.indexOf(contact);
+        }
+    }
+    let msg = "Your change is saved!";
     let contact_edit = document.getElementById("contact_edit");
     contact_edit.innerHTML = `
   <div class="contact_new_top">
@@ -147,26 +178,33 @@ function editContact() {
     <span>Edit contact</span>  
   </div> 
   <form class="contact_new_bottom" id="edit_contact" onsubmit="onFormSubmit();"> 
-    <input type="text" placeholder="Name" name="name" maxlength="36" id="name"> 
-    <input type="text" placeholder="Surname" name="surname" maxlength="36" id="surname"> 
-    <input type="email" placeholder="Email" name="mail" maxlength="36" id="mail"> 
-    <input type="tel" placeholder="Phone" name="phone" id="phone" pattern="[0-9]{10,16}" title="Please enter a valid phone number (between 10 and 16 digits)"> 
-    <div class="contact_new_btns"> 
-      <button type="submit" class="button_dark" onclick="saveNewContact();showConfirmationAddContact(a=true);" value="Add">Save</button> 
+    <input type="text" placeholder="Name" name="name" maxlength="36" id="edit_name" value="${name}"> 
+    <input type="text" placeholder="Surname" name="surname" maxlength="36" id="edit_surname" value="${surname}"> 
+    <input type="email" placeholder="Email" name="mail" maxlength="36" id="edit_mail" value="${mail}"> 
+    <input type="tel" placeholder="Phone" name="phone" id="edit_phone" pattern="[0-9]{10,16}" 
+      title="Please enter a valid phone number (between 10 and 16 digits)" value="${phone}"> 
+    <div class="contact_new_btns">
+      <button type="submit" class="button_dark" 
+        onclick="editExistingContact();
+          showConfirmationContact(true,'${msg}');">Save
+      </button> 
     </div> 
   </form>
 `;
 }
 
-function showConfirmationAddContact(a) {
-    let contact_added_confirmation = document.getElementById(
-        "contact_added_confirmation"
+function showConfirmationContact(a, msg) {
+    console.log(msg);
+    let confirm_msg = document.getElementById('confirm_msg');
+    confirm_msg.innerHTML = msg;
+    let contact_confirmation = document.getElementById(
+        "contact_confirmation"
     );
     if (a) {
-        contact_added_confirmation.style.visibility = "visible";
+        contact_confirmation.style.visibility = "visible";
     }
     if (!a) {
-        contact_added_confirmation.style.visibility = "hidden";
+        contact_confirmation.style.visibility = "hidden";
     }
 }
 
