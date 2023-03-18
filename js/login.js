8f899ac49186b2efcf133330f35f885fb0878177
HasanAltay <altay@posteo.de>
2023-03-18 21:51:12 +0100

    login inputs disabled for better user experience

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ function login() {
                     class="login_input" name="email" required autocomplete="email" value="sabine@mustermann.de">
                 <img src="./img/mail.png" class="login_mail_png">
                 <input placeholder="Password" id="password_login" type="password" 
-                    class="login_input" name="password" required autocomplete="current-password" value="sabine123">
+                    class="login_input" name="password" required autocomplete="current-password" value="sabine123" disabled>
                 <img src="./img/secure.png" class="login_secure_png">  
                 </div>
                 <div class="login_forgot">
@@ -38,7 +38,7 @@ function forgotPassword() {
         </span>
         <form onsubmit="resetPassword()">
             <div class="ai-center jc-center d-flex fd-col pos-rel">
-                <input placeholder="Email" id="email_forgot" type="email" class="login_input" name="email_forgot" required>
+                <input placeholder="Email" id="email_forgot" type="email" class="login_input" name="email_forgot" required disabled>
                 <img src="./img/mail.png" class="login_mail_png">
                 <button class="button_dark" type="submit" name="sendmail">Send me the email</button>
             </div>
@@ -55,14 +55,14 @@ function signUp() {
         <div class="login_blue_line"></div>
         <form onsubmit="registerUser()">
             <div class="login_pos_rel">
-                <input placeholder="Name" id="username" type="text" class="login_input">
+                <input placeholder="Name" id="username" type="text" class="login_input" disabled>
                 <img src="./img/name.png" class="sign_name_png">
-                <input placeholder="Email" id="email" type="email" class="login_input" required>
+                <input placeholder="Email" id="email" type="email" class="login_input" required disabled>
                 <img src="./img/mail.png" class="sign_mail_png">
-                <input placeholder="Password" id="password" type="password" class="login_input" required>
+                <input placeholder="Password" id="password" type="password" class="login_input" required disabled>
                 <img src="./img/secure.png" class="sign_secure_png">
                 <div>
-                    <button type="submit" class="button_dark">Sign up</button>
+                    <button type="submit" class="button_dark" disabled>Sign up</button>
                 </div>
             </div>
         </form>
@@ -78,14 +78,14 @@ function resetPassword() {
         <div class="login_blue_line"></div>
         <span class="font21-400 txt-cr mb-40">Change your account password</span>
         <div class="login_pos_rel">
-            <input placeholder="New password" id="new_password" type="password" class="login_input" autocomplete="new-password">
+            <input placeholder="New password" id="new_password" type="password" class="login_input" autocomplete="new-password" disabled>
         </div>
         <div class="login_pos_rel">
-            <input placeholder="Confirm password" id="confirm_password" type="password" class="login_input">
+            <input placeholder="Confirm password" id="confirm_password" type="password" class="login_input" disabled>
         </div>
         <br>
         <div>
-            <button class="button_dark" onclick="hideLogin()">Continue</button>
+            <button class="button_dark" onclick="hideLogin()" disabled>Continue</button>
         </div>
 `;
 }
